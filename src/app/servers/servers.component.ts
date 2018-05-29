ec0494e16c177578bc1c51d115ac7bb73fc682c1
Vikas Kumar <negi.vikash925@gmail.com>
2018-05-29 19:36:02 +0530

    Passing and using date with event binding and Two Way databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 	allowNewServer = false;
 	serverCreationStatus = "No server was created!";
+	serverName = 'Test Srver';
 	constructor() {
 		setTimeout(() =>{
 			this.allowNewServer = true;
@@ -17,7 +18,10 @@ export class ServersComponent implements OnInit {
 	ngOnInit() {
 	}
 	onCreateServer(){
-		this.serverCreationStatus = "Server was created!";
+		this.serverCreationStatus = "Server was created!.Name is "+this.serverName;
+	}
+	onUpdateServerName(event:Event){
+		this.serverName = (<HTMLInputElement>event.target).value;
 	}
 
 }
