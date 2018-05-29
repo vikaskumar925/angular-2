d218f2e94dcd966b5dc110bbb8fed5ca0f413c57
Vikas Kumar <negi.vikash925@gmail.com>
2018-05-29 21:14:22 +0530

    Angular 2 built in directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 	allowNewServer = false;
 	serverCreationStatus = "No server was created!";
-	serverName = 'Test Srver';
+	serverName = 'Test Server';
+	serverCreated = false;
+	servers = ['Test Server', 'Test Server2'];
 	constructor() {
 		setTimeout(() =>{
 			this.allowNewServer = true;
@@ -18,6 +20,8 @@ export class ServersComponent implements OnInit {
 	ngOnInit() {
 	}
 	onCreateServer(){
+		this.serverCreated = true;
+		this.servers.push(this.serverName);
 		this.serverCreationStatus = "Server was created!.Name is "+this.serverName;
 	}
 	onUpdateServerName(event:Event){
